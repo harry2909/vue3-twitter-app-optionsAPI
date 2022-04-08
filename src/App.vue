52ec96bf83a8bcc3d9ac6f5c06389f1527fd5e9c
Harry Preston <47326455+harry2909@users.noreply.github.com>
2022-04-08 10:56:09 +0100

    Basic data object, computed function and method to display data + add followers on button press

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
-  <div id="app">
-    {{user.username}}
+  <div id="app" style="display:flex; flex-direction:column; justify-content: center; align-items: center">
+    @{{user.username}} - {{fullName}}
+    <br>
+    <strong>Followers: {{followers}}</strong>
+    <button style="width:100px" @click="followUser">Follow</button>
   </div>
 </template>
 
@@ -20,5 +23,15 @@ export default {
       }
     };
   },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`;
+    }
+  },
+  methods: {
+    followUser() {
+      this.followers++;
+    }
+  }
 };
 </script>
