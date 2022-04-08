0580f3cacb507845c22ccf61a4fdd8f9cd85f667
Harry Preston <47326455+harry2909@users.noreply.github.com>
2022-04-08 10:44:44 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    {{user.username}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: 'Harry_Preston',
+        firstName: 'Harry',
+        lastName: 'Preston',
+        email: 'harry@gmail.com',
+        isAdmin: true
+      }
+    };
+  },
+};
+</script>
