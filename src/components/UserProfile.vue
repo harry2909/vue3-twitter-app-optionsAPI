42526acc3ce2223e3a18e25c8b6e90cfd53efb99
Harry Preston <47326455+harry2909@users.noreply.github.com>
2022-04-08 12:20:17 +0100

    ability to add tweets

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -6,6 +6,23 @@
       <div class="user-profile__follower-count">
         <strong>Followers: </strong> {{ followers }}
       </div>
+      <form class="user-profile__create-twoot" @submit.prevent="createNewTwoot">
+        <label for="newTwoot"><strong>New Twoot</strong></label>
+        <textarea id="newTwoot" rows="4" v-model="newTwootContent"></textarea>
+      <div class="user-profile__create-twoot-type">
+        <label for="newTwootType"><strong>Type: </strong></label>
+        <select id="newTwootType" v-model="selectedTwootType">
+          <option
+            :value="option.value"
+            v-for="(option, index) in twootTypes"
+            :key="index"
+          >
+            {{ option.name }}
+          </option>
+        </select>
+      </div>
+      <button>Twoot!</button>
+       </form>
     </div>
     <div class="user-profile__twoots-wrapper">
       <TwootItem
@@ -27,6 +44,12 @@ export default {
   components: { TwootItem },
   data() {
     return {
+      newTwootContent: "",
+      selectedTwootType: "instant",
+      twootTypes: [
+        { value: "draft", name: "Draft" },
+        { value: "instant", name: "Instant Twoot" },
+      ],
       followers: 0,
       user: {
         id: 1,
@@ -59,8 +82,16 @@ export default {
       this.followers++;
     },
     toggleFavourite(id) {
-        console.log(`Favourited Tweet ${id}`)
-    }
+      console.log(`Favourited Tweet ${id}`);
+    },
+    createNewTwoot() {
+      if (this.newTwootContent && this.selectedTwootType !== "draft") {
+        this.user.twoots.unshift({
+          id: this.user.twoots.length + 1,
+          content: this.newTwootContent
+        });
+      }
+    },
   },
   mounted() {
     this.followUser();
